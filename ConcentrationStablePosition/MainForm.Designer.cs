b4a256dde6b2158bc3ed43cf39172e40a8491092
SmeshMike <smeshmike@yandex.ru>
2020-12-04 23:22:47 +0300

    f

--- ConcentrationStablePosition/MainForm.Designer.cs
+++ ConcentrationStablePosition/MainForm.Designer.cs
@@ -33,17 +33,19 @@ namespace ConcentrationStablePosition
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.fiLabel = new System.Windows.Forms.Label();
-            this.fiStartTextBox = new System.Windows.Forms.TextBox();
             this.ndTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.lTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.dfi0TextBox = new System.Windows.Forms.TextBox();
+            this.dfiStartTextBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.dfiLTextBox = new System.Windows.Forms.TextBox();
+            this.dfiEndTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.runButton = new System.Windows.Forms.Button();
+            this.fiEndTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.fiStartTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -63,25 +65,9 @@ namespace ConcentrationStablePosition
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
-            // fiLabel
-            // 
-            this.fiLabel.AutoSize = true;
-            this.fiLabel.Location = new System.Drawing.Point(830, 13);
-            this.fiLabel.Name = "fiLabel";
-            this.fiLabel.Size = new System.Drawing.Size(18, 13);
-            this.fiLabel.TabIndex = 1;
-            this.fiLabel.Text = "fi0";
-            // 
-            // fiStartTextBox
-            // 
-            this.fiStartTextBox.Location = new System.Drawing.Point(797, 29);
-            this.fiStartTextBox.Name = "fiStartTextBox";
-            this.fiStartTextBox.Size = new System.Drawing.Size(51, 20);
-            this.fiStartTextBox.TabIndex = 2;
-            // 
             // ndTextBox
             // 
-            this.ndTextBox.Location = new System.Drawing.Point(797, 72);
+            this.ndTextBox.Location = new System.Drawing.Point(797, 29);
             this.ndTextBox.Name = "ndTextBox";
             this.ndTextBox.Size = new System.Drawing.Size(51, 20);
             this.ndTextBox.TabIndex = 4;
@@ -89,7 +75,7 @@ namespace ConcentrationStablePosition
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(802, 56);
+            this.label2.Location = new System.Drawing.Point(802, 13);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 3;
@@ -97,7 +83,7 @@ namespace ConcentrationStablePosition
             // 
             // lTextBox
             // 
-            this.lTextBox.Location = new System.Drawing.Point(797, 114);
+            this.lTextBox.Location = new System.Drawing.Point(797, 71);
             this.lTextBox.Name = "lTextBox";
             this.lTextBox.Size = new System.Drawing.Size(51, 20);
             this.lTextBox.TabIndex = 6;
@@ -105,39 +91,39 @@ namespace ConcentrationStablePosition
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(835, 98);
+            this.label3.Location = new System.Drawing.Point(835, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(13, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "L";
             // 
-            // dfi0TextBox
+            // dfiStartTextBox
             // 
-            this.dfi0TextBox.Location = new System.Drawing.Point(797, 159);
-            this.dfi0TextBox.Name = "dfi0TextBox";
-            this.dfi0TextBox.Size = new System.Drawing.Size(51, 20);
-            this.dfi0TextBox.TabIndex = 8;
+            this.dfiStartTextBox.Location = new System.Drawing.Point(797, 116);
+            this.dfiStartTextBox.Name = "dfiStartTextBox";
+            this.dfiStartTextBox.Size = new System.Drawing.Size(51, 20);
+            this.dfiStartTextBox.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(821, 143);
+            this.label4.Location = new System.Drawing.Point(821, 100);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(27, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "fi`(0)";
             // 
-            // dfiLTextBox
+            // dfiEndTextBox
             // 
-            this.dfiLTextBox.Location = new System.Drawing.Point(797, 203);
-            this.dfiLTextBox.Name = "dfiLTextBox";
-            this.dfiLTextBox.Size = new System.Drawing.Size(51, 20);
-            this.dfiLTextBox.TabIndex = 10;
+            this.dfiEndTextBox.Location = new System.Drawing.Point(797, 160);
+            this.dfiEndTextBox.Name = "dfiEndTextBox";
+            this.dfiEndTextBox.Size = new System.Drawing.Size(51, 20);
+            this.dfiEndTextBox.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(821, 187);
+            this.label1.Location = new System.Drawing.Point(821, 144);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 13);
             this.label1.TabIndex = 9;
@@ -152,22 +138,56 @@ namespace ConcentrationStablePosition
             this.runButton.Text = "Run";
             this.runButton.UseVisualStyleBackColor = true;
             // 
+            // fiEndTextBox
+            // 
+            this.fiEndTextBox.Location = new System.Drawing.Point(797, 246);
+            this.fiEndTextBox.Name = "fiEndTextBox";
+            this.fiEndTextBox.Size = new System.Drawing.Size(51, 20);
+            this.fiEndTextBox.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(821, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "fi(L)";
+            // 
+            // fiStartTextBox
+            // 
+            this.fiStartTextBox.Location = new System.Drawing.Point(797, 202);
+            this.fiStartTextBox.Name = "fiStartTextBox";
+            this.fiStartTextBox.Size = new System.Drawing.Size(51, 20);
+            this.fiStartTextBox.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(821, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "fi(0)";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(854, 462);
+            this.Controls.Add(this.fiEndTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fiStartTextBox);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.runButton);
-            this.Controls.Add(this.dfiLTextBox);
+            this.Controls.Add(this.dfiEndTextBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dfi0TextBox);
+            this.Controls.Add(this.dfiStartTextBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.lTextBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.ndTextBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.fiStartTextBox);
-            this.Controls.Add(this.fiLabel);
             this.Controls.Add(this.chart1);
             this.Name = "MainForm";
             this.Text = "Form1";
@@ -180,17 +200,19 @@ namespace ConcentrationStablePosition
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Label fiLabel;
-        private System.Windows.Forms.TextBox fiStartTextBox;
         private System.Windows.Forms.TextBox ndTextBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox lTextBox;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox dfi0TextBox;
+        private System.Windows.Forms.TextBox dfiStartTextBox;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox dfiLTextBox;
+        private System.Windows.Forms.TextBox dfiEndTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox fiEndTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox fiStartTextBox;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- ConcentrationStablePosition/MathsStuff.cs
+++ ConcentrationStablePosition/MathsStuff.cs
@@ -1,56 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConcentrationStablePosition
 {
     class MathsStuff
     {
-        private double _nd, _t, _environmentEpsilon, _u0, _l, _n0, _epsilon, _fi, _q, _dfiStart, _dfiEnd, _fiStart, _fiEnd, _kx, _b;
+        private double _nd, _t, _u0, _l, _n0,  _q, _dfiStart, _dfiEnd, _fiStart, _fiEnd, _b, _step;
         private readonly double _k = 1.380649 * Math.Pow(10, -23);
         private readonly double _e = - 1.602176 * Math.Pow(10, -19);
         private readonly double _epsilon0 = 8.854187 * Math.Pow(10, -12);
-
+        private List<double> _fi = new List<double>(1000);
 
 
 
         public double T { get; set; }
         public double EnvironmentEpsilon { get; set; }
         public double U0 { get; set; }
-        public double L { get; set; }
+        public double L { get =>_l; set => _l = value; }
 
-        private double Fi0 => _k * _t / _q;
-        private double Xi0 => Math.Sqrt(_k * _t * _epsilon0 * _epsilon/ (_q*_q));
         private double Nd => _nd * _n0;
 
         double Q(double fi, double nd)
         {
-            return fi * Math.Pow(Math.E, fi) * nd;
+            return -fi *Math.Pow(Math.E, -fi) * nd;
         }
 
         double R(double fi, double nd)
         {
-            return Q(fi, nd)*fi + nd*(1 - Math.Pow(Math.E, fi));
+            return Q(fi, nd)*fi - nd*(1 - Math.Pow(Math.E, -fi));
         }
 
-        private double _k1, _k2, _k3, _k4, _z, _alpha, _beta;
-
-        double K(double x)
+        void InitFi()
         {
-            return _kx * x + _b;
+            var tmpK = - 2 * _fiStart / 1000;
+            for (int i = 0; i < 1000; i++)
+            {
+                _fi.Add(tmpK * i + _fiStart);
+            }
         }
 
-
-        void ChasingMethod(double start, double end, int direction, double step)
+        private List<double> _alpha;
+        private List<double> _beta;
+        void ChasingMethodForward()
         {
+            double k1, k2, k3, k4, q1, q2, q3, q4;
+            List<double> alpha = new List<double>() {0};
+            List<double> beta = new List<double>() {_dfiStart};
+            for (int i = 0; i < 999; i++)
+            {
+                //k1 = Q(_fi[i], _nd) - alpha[i]* alpha[i];
+                //k2  = Q(_fi[i]+ _step/2, _nd) - (alpha[i] + _step/2*k1) * (alpha[i] + _step / 2 * k1);
+                //k3 = Q(_fi[i] + _step / 2, _nd) - (alpha[i] + _step / 2 * k2) * (alpha[i] + _step / 2 * k2);
+                //k4 = Q(_fi[i] + _step, _nd) - (alpha[i] + _step * k1) * (alpha[i] + _step * k1);
+                //alpha.Add(alpha[i] + _step/6*(k1 + 2*k2 + 2*k3 + k4));
 
-            _k1 = _q;
-            _k2 = _q + step/2 - _k1*(step/2)* (step / 2);
-            _k3 = _q + step / 2 - _k2 * step / 2;
-            _k4 = _q + step - ;
+                k1 = Q(_fi[i], _nd) - alpha[i] * alpha[i];
+                q1 = R(_fi[i], _nd) - alpha[i] * beta[i];
+                k2 = Q(_fi[i] + k1* _step / 2, _nd) - (alpha[i] + _step / 2 * q1) * (alpha[i] + _step / 2 * q1);
+                q2 = R(_fi[i] + k1* _step / 2, _nd) - (alpha[i] + _step / 2 * q1) * (beta[i] + _step / 2 * q1);
+                k3 = Q(_fi[i] + k2 * _step / 2, _nd) - (alpha[i] + _step / 2 * q2) * (alpha[i] + _step / 2 * q2);
+                q3 = R(_fi[i] + k2 * _step / 2, _nd) - (alpha[i] + _step / 2 * q2) * (beta[i] + _step / 2 * q2);
+                k4 = Q(_fi[i] + k3*_step, _nd) - (alpha[i] + _step * q3) * (alpha[i] + _step * q3);
+                q4 = R(_fi[i] + k3 * _step, _nd) - (alpha[i] + _step * q3) * (beta[i] + _step * q3);
+                alpha.Add(alpha[i] + _step / 6 * (k1 + 2 * k2 + 2 * k3 + k4));
+                beta.Add(beta[i] + _step / 6 * (q1 + 2 * q2 + 2 * q3 + q4));
+            }
+
+            _alpha = alpha;
+            _beta = beta;
         }
+        void ChasingMethodBack()
+        {
+            _fi[^1] = (_dfiEnd - _beta[_beta.Count]) / _alpha[_alpha.Count];
+            for (int i = _fi.Length - 2; i >= 0; i++)
+            {
+                _fi[i] = 
+            }
+            double k1, k2, k3, k4, q1, q2, q3, q4;
+            List<double> alpha = new List<double>() { 0 };
+            List<double> beta = new List<double>() { _dfiStart };
+            for (int i = 0; i < _fi.Length - 1; i++)
+            {
+                //k1 = Q(_fi[i], _nd) - alpha[i]* alpha[i];
+                //k2  = Q(_fi[i]+ _step/2, _nd) - (alpha[i] + _step/2*k1) * (alpha[i] + _step / 2 * k1);
+                //k3 = Q(_fi[i] + _step / 2, _nd) - (alpha[i] + _step / 2 * k2) * (alpha[i] + _step / 2 * k2);
+                //k4 = Q(_fi[i] + _step, _nd) - (alpha[i] + _step * k1) * (alpha[i] + _step * k1);
+                //alpha.Add(alpha[i] + _step/6*(k1 + 2*k2 + 2*k3 + k4));
 
+                k1 = Q(_fi[i], _nd) - alpha[i] * alpha[i];
+                q1 = R(_fi[i], _nd) - alpha[i] * beta[i];
+                k2 = Q(_fi[i] + k1 * _step / 2, _nd) - (alpha[i] + _step / 2 * q1) * (alpha[i] + _step / 2 * q1);
+                q2 = R(_fi[i] + k1 * _step / 2, _nd) - (alpha[i] + _step / 2 * q1) * (beta[i] + _step / 2 * q1);
+                k3 = Q(_fi[i] + k2 * _step / 2, _nd) - (alpha[i] + _step / 2 * q2) * (alpha[i] + _step / 2 * q2);
+                q3 = R(_fi[i] + k2 * _step / 2, _nd) - (alpha[i] + _step / 2 * q2) * (beta[i] + _step / 2 * q2);
+                k4 = Q(_fi[i] + k3 * _step, _nd) - (alpha[i] + _step * q3) * (alpha[i] + _step * q3);
+                q4 = R(_fi[i] + k3 * _step, _nd) - (alpha[i] + _step * q3) * (beta[i] + _step * q3);
+                alpha.Add(alpha[i] + _step / 6 * (k1 + 2 * k2 + 2 * k3 + k4));
+                beta.Add(beta[i] + _step / 6 * (q1 + 2 * q2 + 2 * q3 + q4));
+            }
+
+        }
     }
 }
